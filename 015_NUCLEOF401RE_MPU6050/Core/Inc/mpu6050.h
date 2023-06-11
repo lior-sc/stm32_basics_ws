9712c5444e5b7dc1e8b0a7118acf3ada65441de8
Soltrex-io <lior@soltrex.io>
2023-06-11 19:13:28 +0300

    015 | init project

---
+++ 015_NUCLEOF401RE_MPU6050/Core/Inc/mpu6050.h
@@ -0,0 +1,13 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: Jun 11, 2023
+ *      Author: lior_sc
+ */
+
+#ifndef _MPU6050_H_
+#define _MPU6050_H_
+
+#include "i2c.h"
+
+#endif /* _MPU6050_H_ */
---
+++ 015_NUCLEOF401RE_MPU6050/Core/Src/mpu6050.c
@@ -0,0 +1,23 @@
+/*
+ * mpu6050.c
+ *
+ *  Created on: Jun 11, 2023
+ *      Author: lior_sc
+ */
+
+#include "mpu6050.h"
+
+/**
+ * @note
+ * links to reference libraries:
+ * 1. https://github.com/leech001/MPU6050 (this is a good C library)
+ * 2. https://github.com/xstackman/MPU6050_Library_STM32/tree/main (C++ library but with good defines)
+ *
+ * links to reference manuals
+ * 1. MPU6050 datasheet: https://pdf1.alldatasheet.com/datasheet-pdf/view/1132807/TDK/MPU-6050.html
+ * 2. Nucleo F401RE pinout: https://os.mbed.com/platforms/ST-Nucleo-F401RE/
+ */
+
+#define
+
+
